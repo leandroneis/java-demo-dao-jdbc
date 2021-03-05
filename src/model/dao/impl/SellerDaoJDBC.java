177619d46adcdb3737c703f4ba84eeddec39f42b
Leandro Gustavo Neis <lgn200@gmail.com>
2021-03-04 22:57:44 -0300

    delete implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -56,19 +56,18 @@ public class SellerDaoJDBC implements SellerDao {
         try {
             st = conn.prepareStatement(
                     " UPDATE seller "
-                    + " SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
-                    + " WHERE Id = ?");
+                            + " SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+                            + " WHERE Id = ?");
 
             st.setString(1, obj.getName());
             st.setString(2, obj.getEmail());
             st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
             st.setDouble(4, obj.getBaseSalary());
             st.setInt(5, obj.getDepartment().getId());
-            st.setInt(6,obj.getId());
+            st.setInt(6, obj.getId());
 
             st.executeUpdate();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             throw new DbException(e.getMessage());
         } finally {
             DB.closeStatement(st);
@@ -77,7 +76,25 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void deletById(Integer id) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+            st.setInt(1, id);
+
+
+            int rows = st.executeUpdate();
+
+            if(rows == 0){
+                throw new DbException("Id not found or not exists");
+            }
+
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
